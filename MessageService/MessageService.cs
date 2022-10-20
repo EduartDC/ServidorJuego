c0ce049c91b6da4bb97748f85b0eaa383290b426
EDUART USSIEL DIRCIO CAYON <zS19030173@estudiantes.uv.mx>
2022-10-19 22:40:19 -0500

    Se lipio el codigo, dejando el codigo que se utilizara dentro del proyecto.

--- MessageService/MessageService.cs
+++ MessageService/MessageService.cs
@@ -11,28 +11,7 @@ namespace MessageService
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class MessageService : IUserManager
     {
-        public int AddManager(Manager manager)
-        {
-            Console.WriteLine("Adding manager ..{0},{1}", manager.UserName, manager.LastName);
-            return 1;
-            //código que se conecta con la BD y guarda la información de manager
-        }
 
-        public int AddUser(User user)
-        {
-           Console.WriteLine("Adding user...{0},{1}",user.UserName,user.LastName);
-
-            return 1;
-        }
-
-        public User GetUserById(string userId)
-        {
-            return new User
-            {
-                 LastName="Pérez Arriaga",
-                 UserName="Juan Carlos"
-            };
-        }
         public int AddPlayer(Player newPlayer)
         {
             var result = 0;
